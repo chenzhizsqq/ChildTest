e435c3bd25263ef2056d50a0f568f24f6d76a0bf
chenzhi <chenzhi@qq.com>
2021-11-24 17:21:34 +0900

    加上指纹认证

--- app/src/main/java/com/example/childtest/LoginActivity.kt
+++ app/src/main/java/com/example/childtest/LoginActivity.kt
@@ -1,13 +1,22 @@
 package com.example.childtest
 
 import android.app.KeyguardManager
+import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
+import android.hardware.biometrics.BiometricManager
+import android.hardware.biometrics.BiometricPrompt
+import android.os.Build
 import android.os.Bundle
+import android.os.CancellationSignal
 import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import com.example.childtest.databinding.ActivityLoginBinding
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
     val TAG = "LoginActivity"
@@ -27,12 +36,11 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if (bWelcomePassword == true) {
-            mKeyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
-            showScreenLockPwd()
+        mKeyguardManager = getSystemService(KEYGUARD_SERVICE) as KeyguardManager
 
+        if (bWelcomePassword == true) {
             binding.root.setOnClickListener {
-                showScreenLockPwd()
+                checkCredential(this@LoginActivity)
             }
         } else {
             val intent =
@@ -41,6 +49,58 @@ class LoginActivity : AppCompatActivity() {
             finish()
         }
     }
+
+
+    private fun checkCredential(context: Context) {
+        val keyguardManager =
+            context.getSystemService(Context.KEYGUARD_SERVICE) as? KeyguardManager
+        //端末認証が有効か?
+        if (keyguardManager?.isKeyguardSecure == true) {
+            //OS10以上か?
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                val biometricPrompt =
+                    //OS11以上か?
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+                        BiometricPrompt.Builder(context)
+                            .setTitle("手机认证")
+                            .setAllowedAuthenticators(BiometricManager.Authenticators.DEVICE_CREDENTIAL)
+                            .build()
+                    } else {
+                        BiometricPrompt.Builder(context)
+                            .setTitle("手机认证")
+                            .setDeviceCredentialAllowed(true)
+                            .build()
+                    }
+
+                //認証のキャンセル要求があった
+                val cancellationSignal = CancellationSignal()
+                cancellationSignal.setOnCancelListener {
+                    //キャンセルされた
+                }
+
+                val executors = ContextCompat.getMainExecutor(context)
+                val authCallBack = object : BiometricPrompt.AuthenticationCallback() {
+                    override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult?) {
+                        //認証成功
+                        val intent =
+                            Intent(this@LoginActivity, MenuActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
+
+                    override fun onAuthenticationError(errorCode: Int, errString: CharSequence?) {
+                        //認証失敗 or ダイアログを消す
+                    }
+                }
+                biometricPrompt.authenticate(cancellationSignal, executors, authCallBack)
+            } else {
+
+                showScreenLockPwd()
+
+            }
+        }
+    }
+
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -61,6 +121,7 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
+    //手机密码验证
     private fun showScreenLockPwd() {
         val intent: Intent = mKeyguardManager.createConfirmDeviceCredentialIntent(null, null)
         startActivityForResult(intent, 2)
