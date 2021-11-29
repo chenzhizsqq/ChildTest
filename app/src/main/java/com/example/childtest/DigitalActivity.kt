91206c0a26407d327762ec0cc00c8af88c28bcf6
chenzhi <chenzhi@qq.com>
2021-11-29 18:27:28 +0900

    添加最大数的设置，和随机答案

--- app/src/main/java/com/example/childtest/DigitalActivity.kt
+++ app/src/main/java/com/example/childtest/DigitalActivity.kt
@@ -1,16 +1,13 @@
 package com.example.childtest
 
 import android.content.SharedPreferences
-import android.graphics.Color
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.util.Size
 import androidx.preference.PreferenceManager
 import com.example.childtest.databinding.ActivityDigitalBinding
 import java.util.*
 import android.widget.TextView
-
-
+import androidx.preference.PreferenceFragmentCompat
 
 
 class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener {
@@ -35,6 +32,14 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener {
         binding = ActivityDigitalBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.digital_settings, DigitalSettingsFragment())
+                .commit()
+        }
+
         // TextToSpeechの生成
         this.tts = TextToSpeech(this, this)
 
@@ -57,28 +62,58 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener {
     }
 
     private fun initNumber() {
-        var randomNum1 = Tools.randomNum(1, 3)
-        var randomNum2 = Tools.randomNum(1, 3)
+        var randomMax = sharedPreferences.getInt("digital_preferences_max_num", 3)
+        var randomNum1 = Tools.randomNum(1, randomMax)
+        var randomNum2 = Tools.randomNum(1, randomMax)
         while (
             binding.number1.text == randomNum1.toString() ||
             binding.number2.text == randomNum2.toString()
         ){
-            randomNum1 = Tools.randomNum(1, 3)
-            randomNum2 = Tools.randomNum(1, 3)
+            randomNum1 = Tools.randomNum(1, randomMax)
+            randomNum2 = Tools.randomNum(1, randomMax)
         }
         binding.number1.text = randomNum1.toString()
         binding.number2.text = randomNum2.toString()
 
         //ll_image_1    ll_image_2
-        binding.llImage1.removeAllViews()
-        binding.llImage2.removeAllViews()
+/*        binding.llImage1.removeAllViews()
+        binding.llImage2.removeAllViews()*/
 
-        for (i in 0 until randomNum1) {
+/*        for (i in 0 until randomNum1) {
             binding.llImage1.addView(createText(Color.RED))
         }
         for (i in 0 until randomNum2) {
             binding.llImage2.addView(createText(Color.BLUE))
+        }*/
+
+
+        binding.tvNum1.text = addViewText(randomNum1)
+        binding.tvNum2.text = addViewText(randomNum2)
+
+        val answer = randomNum1 + randomNum2
+
+        var answer2 = answer + Tools.randomNum(-2, 2)
+        while (answer == answer2){
+            answer2 = answer + Tools.randomNum(-2, 2)
         }
+
+        var answer3 = answer + Tools.randomNum(1, 3)
+        while (answer == answer3
+            || answer2 == answer3){
+            answer3 = answer + Tools.randomNum(1, 3)
+        }
+
+        val answerList = listOf(answer2 , answer ,answer3)
+
+        val startInt = Tools.randomNum(0,2)
+
+        val answers_random1 = answerList[startInt]
+        val answers_random2 = answerList[(startInt+1)%3]
+        val answers_random3 = answerList[(startInt+2)%3]
+
+        binding.answer1.text = answers_random1.toString()
+        binding.answer2.text = answers_random2.toString()
+        binding.answer3.text = answers_random3.toString()
 
         if (bClickedRead == true) {
             this.tts!!.speak(
@@ -88,6 +123,14 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener {
                 "utteranceId"
             )
         }
+    }
+
+    private fun addViewText(randomNum: Int):String {
+        var r = ""
+        for (i in 0 until randomNum) {
+            r += "〇"
+        }
+        return r
     }
 
     private fun createText(color:Int):TextView {
@@ -126,4 +169,10 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener {
             }
         }
     }
+    class DigitalSettingsFragment : PreferenceFragmentCompat() {
+        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+            setPreferencesFromResource(R.xml.digital_preferences, rootKey)
+        }
+    }
 }
+
