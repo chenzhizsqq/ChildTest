a86411a8a84716ad136a37a6f532eeffeb7d42c9
chenzhi <chenzhi@qq.com>
2021-11-30 11:12:23 +0900

    整理

--- app/src/main/java/com/example/childtest/DigitalActivity.kt
+++ app/src/main/java/com/example/childtest/DigitalActivity.kt
@@ -3,17 +3,15 @@ package com.example.childtest
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.speech.tts.TextToSpeech
-import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
 import com.example.childtest.databinding.ActivityDigitalBinding
 import java.util.*
-import android.widget.TextView
-import android.widget.Toast
-import androidx.preference.PreferenceFragmentCompat
 
 
-class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClickListener {
+class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener, View.OnClickListener {
     private var tts: TextToSpeech? = null
 
     private lateinit var binding: ActivityDigitalBinding
@@ -48,12 +46,12 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
         this.tts = TextToSpeech(this, this)
 
 
-        binding.llText.setOnClickListener (this)
-        binding.nextTest.setOnClickListener (this)
+        binding.llText.setOnClickListener(this)
+        binding.nextTest.setOnClickListener(this)
 
-        binding.answer1.setOnClickListener (this)
-        binding.answer2.setOnClickListener (this)
-        binding.answer3.setOnClickListener (this)
+        binding.answer1.setOnClickListener(this)
+        binding.answer2.setOnClickListener(this)
+        binding.answer3.setOnClickListener(this)
 
         initNumber()
     }
@@ -72,30 +70,30 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
                 )
             }
             R.id.answer_1 -> {
-                if (binding.answer1.text.toString() == currentAnswer.toString()){
+                if (binding.answer1.text.toString() == currentAnswer.toString()) {
                     Toast.makeText(this, "答对了", Toast.LENGTH_SHORT).show()
                     speakMsg("答对了")
-                }else{
+                } else {
                     Toast.makeText(this, "答错了", Toast.LENGTH_SHORT).show()
                     speakMsg("答错了")
                 }
             }
             R.id.answer_2 -> {
 
-                if (binding.answer2.text.toString() == currentAnswer.toString()){
+                if (binding.answer2.text.toString() == currentAnswer.toString()) {
                     Toast.makeText(this, "答对了", Toast.LENGTH_SHORT).show()
                     speakMsg("答对了")
-                }else{
+                } else {
                     Toast.makeText(this, "答错了", Toast.LENGTH_SHORT).show()
                     speakMsg("答错了")
                 }
             }
             R.id.answer_3 -> {
 
-                if (binding.answer3.text.toString() == currentAnswer.toString()){
+                if (binding.answer3.text.toString() == currentAnswer.toString()) {
                     Toast.makeText(this, "答对了", Toast.LENGTH_SHORT).show()
                     speakMsg("答对了")
-                }else{
+                } else {
                     Toast.makeText(this, "答错了", Toast.LENGTH_SHORT).show()
                     speakMsg("答错了")
                 }
@@ -104,29 +102,18 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
     }
 
     private fun initNumber() {
-        var randomMax = sharedPreferences.getInt("digital_preferences_max_num", 3)
+        val randomMax = sharedPreferences.getInt("digital_preferences_max_num", 3)
         var randomNum1 = Tools.randomNum(1, randomMax)
         var randomNum2 = Tools.randomNum(1, randomMax)
         while (
             binding.number1.text == randomNum1.toString() ||
             binding.number2.text == randomNum2.toString()
-        ){
+        ) {
             randomNum1 = Tools.randomNum(1, randomMax)
             randomNum2 = Tools.randomNum(1, randomMax)
         }
         binding.number1.text = randomNum1.toString()
         binding.number2.text = randomNum2.toString()
-
-        //ll_image_1    ll_image_2
-/*        binding.llImage1.removeAllViews()
-        binding.llImage2.removeAllViews()*/
-
-/*        for (i in 0 until randomNum1) {
-            binding.llImage1.addView(createText(Color.RED))
-        }
-        for (i in 0 until randomNum2) {
-            binding.llImage2.addView(createText(Color.BLUE))
-        }*/
 
 
         binding.tvNum1.text = addViewText(randomNum1)
@@ -135,23 +122,24 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
         currentAnswer = randomNum1 + randomNum2
 
         var answer2 = currentAnswer + Tools.randomNum(-2, 2)
-        while (currentAnswer == answer2){
+        while (currentAnswer == answer2) {
             answer2 = currentAnswer + Tools.randomNum(-2, 2)
         }
 
         var answer3 = currentAnswer + Tools.randomNum(1, 3)
         while (currentAnswer == answer3
-            || answer2 == answer3){
+            || answer2 == answer3
+        ) {
             answer3 = currentAnswer + Tools.randomNum(1, 3)
         }
 
-        val answerList = listOf(answer2 , currentAnswer ,answer3)
+        val answerList = listOf(answer2, currentAnswer, answer3)
 
-        val startInt = Tools.randomNum(0,2)
+        val startInt = Tools.randomNum(0, 2)
 
         val answers_random1 = answerList[startInt]
-        val answers_random2 = answerList[(startInt+1)%3]
-        val answers_random3 = answerList[(startInt+2)%3]
+        val answers_random2 = answerList[(startInt + 1) % 3]
+        val answers_random3 = answerList[(startInt + 2) % 3]
 
         binding.answer1.text = answers_random1.toString()
         binding.answer2.text = answers_random2.toString()
@@ -167,7 +155,7 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
         }
     }
 
-    private fun speakMsg(s:String) {
+    private fun speakMsg(s: String) {
         this.tts!!.speak(
             s,
             TextToSpeech.QUEUE_FLUSH,
@@ -176,7 +164,7 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
         )
     }
 
-    private fun addViewText(randomNum: Int):String {
+    private fun addViewText(randomNum: Int): String {
         var r = ""
         for (i in 0 until randomNum) {
             r += "〇"
@@ -184,18 +172,10 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
         return r
     }
 
-    private fun createText(color:Int):TextView {
-        val textView1 = TextView(this)
-        textView1.text = "〇"
-        textView1.textSize = 40F
-        textView1.setTextColor(color)
-        return textView1
-    }
-
     private fun addTestFun(): String {
         val number1Text: String = binding.number1.text.toString()
         val number2Text: String = binding.number2.text.toString()
-        return number1Text +"加$number2Text=" + how_much
+        return number1Text + "加$number2Text=" + how_much
     }
 
     // TextToSpeechの初期化完了時に呼ばれる
@@ -209,7 +189,7 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
                 val chineseSpeak = sharedPreferences.getString("chineseSpeak", "")
                 tts!!.language = Locale("zh", chineseSpeak)
 
-                if (chineseSpeak == "HK"){
+                if (chineseSpeak == "HK") {
                     how_much = "几多"
                 }
             }
@@ -220,6 +200,7 @@ class DigitalActivity : BaseActivity(), TextToSpeech.OnInitListener,View.OnClick
             }
         }
     }
+
     class DigitalSettingsFragment : PreferenceFragmentCompat() {
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.digital_preferences, rootKey)
--- app/src/main/java/com/example/childtest/MenuActivity.kt
+++ app/src/main/java/com/example/childtest/MenuActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.res.ResourcesCompat
 import com.example.childtest.databinding.ActivityMenuBinding
 import java.util.*
 
