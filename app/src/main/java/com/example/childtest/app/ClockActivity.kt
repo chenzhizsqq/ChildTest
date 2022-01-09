1537777c50d43e6ae8eac0183fb2d5c9a4ad7f88
chenzhizs <chenzhi@qq.com>
2022-01-09 13:10:43 +0900

    初步完成时间选择器

--- app/src/main/java/com/example/childtest/app/ClockActivity.kt
+++ app/src/main/java/com/example/childtest/app/ClockActivity.kt
@@ -1,10 +1,13 @@
 package com.example.childtest.app
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.View
+import android.widget.TimePicker
 import com.example.childtest.databinding.ActivityClockBinding
+import java.util.*
 
-class ClockActivity : BaseActivity(),  View.OnClickListener {
+class ClockActivity : BaseActivity(),  View.OnClickListener, TimePickerDialog.OnTimeSetListener {
     private lateinit var binding: ActivityClockBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,5 +16,31 @@ class ClockActivity : BaseActivity(),  View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
+    }
+
+    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
+
+        var mHourOfDay = hourOfDay % 12
+        if (mHourOfDay == 0 )mHourOfDay = 12
+
+        var mMinute = ""
+
+        if (minute>10){
+            mMinute = minute.toString()
+        }else{
+            mMinute = "0$minute"
+        }
+
+        val str = String.format(Locale.US, "%d:%s", mHourOfDay, mMinute)
+
+        binding.tvAddTime.text = str
+    }
+
+    // タイマー設定ボタンをクリックした時のメソッド
+    fun showTimePickerDialog(v: View) {
+        //TImePickクラス呼び出し
+        val newFragment = TimePick()
+        newFragment.show(supportFragmentManager, "timePicker")
+
     }
 }
---
+++ app/src/main/java/com/example/childtest/app/TimePick.kt
@@ -0,0 +1,32 @@
+package com.example.childtest.app
+
+import android.app.Dialog
+import android.app.TimePickerDialog
+import android.os.Bundle
+import android.widget.TimePicker
+import androidx.fragment.app.DialogFragment
+import java.util.*
+
+
+class TimePick : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        //インスタンス
+        val c = Calendar.getInstance()
+        //最初に、システムの現在の時間と分を取得
+        val hour = c.get(Calendar.HOUR_OF_DAY)
+        val minute = c.get(Calendar.MINUTE)
+
+        return TimePickerDialog(
+            activity,
+            activity as ClockActivity?,//返したいアクティビティをセット
+            hour,
+            minute,
+            false)
+    }
+
+    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+        //
+    }
+}
