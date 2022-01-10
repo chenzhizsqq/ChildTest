0d7cb236543aa5594bcbaceb07576d071af308b3
chenzhizs <chenzhi@qq.com>
2022-01-10 19:18:44 +0900

    修改时钟

--- app/src/main/java/com/example/childtest/app/ClockActivity.kt
+++ app/src/main/java/com/example/childtest/app/ClockActivity.kt
@@ -1,46 +1,49 @@
 package com.example.childtest.app
 
-import android.app.TimePickerDialog
 import android.os.Bundle
-import android.view.View
-import android.widget.TimePicker
+import android.util.Log
 import com.example.childtest.databinding.ActivityClockBinding
 import java.util.*
 
-class ClockActivity : BaseActivity(),  View.OnClickListener, TimePickerDialog.OnTimeSetListener {
+
+class ClockActivity : BaseActivity() {
     private lateinit var binding: ActivityClockBinding
+
+
+    //"11:10"   11:08   1:02
+    fun randomTime(): String {
+
+
+        return ""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityClockBinding.inflate(layoutInflater)
         setContentView(binding.root)
-    }
 
-    override fun onClick(v: View?) {
-    }
+        binding.timePicker1.setIs24HourView(false)
+        binding.timePicker1.setOnTimeChangedListener { view, hourOfDay, minute ->
 
-    override fun onTimeSet(view: TimePicker?, hourOfDay: Int, minute: Int) {
 
-        var mHourOfDay = hourOfDay % 12
-        if (mHourOfDay == 0 )mHourOfDay = 12
+            var mHourOfDay = hourOfDay % 12
+            if (mHourOfDay == 0 )mHourOfDay = 12
 
-        var mMinute = ""
+            var mMinute = ""
 
-        if (minute>10){
-            mMinute = minute.toString()
-        }else{
-            mMinute = "0$minute"
+            if (minute>9){
+                mMinute = minute.toString()
+            }else{
+                mMinute = "0$minute"
+            }
+
+            val str = String.format(Locale.US, "%d:%s", mHourOfDay, mMinute)
+
+            binding.tvAddTime.text = str
+            Log.e("TAG", "onCreate: "+str )
         }
 
-        val str = String.format(Locale.US, "%d:%s", mHourOfDay, mMinute)
-
-        binding.tvAddTime.text = str
     }
 
-    // タイマー設定ボタンをクリックした時のメソッド
-    fun showTimePickerDialog(v: View) {
-        //TImePickクラス呼び出し
-        val newFragment = TimePick()
-        newFragment.show(supportFragmentManager, "timePicker")
 
-    }
 }
--- app/src/main/java/com/example/childtest/app/TimePick.kt
+++ app/src/main/java/com/example/childtest/app/TimePick.kt
@@ -7,26 +7,26 @@ import android.widget.TimePicker
 import androidx.fragment.app.DialogFragment
 import java.util.*
 
-
-class TimePick : DialogFragment(), TimePickerDialog.OnTimeSetListener {
-
-
-    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        //インスタンス
-        val c = Calendar.getInstance()
-        //最初に、システムの現在の時間と分を取得
-        val hour = c.get(Calendar.HOUR_OF_DAY)
-        val minute = c.get(Calendar.MINUTE)
-
-        return TimePickerDialog(
-            activity,
-            activity as ClockActivity?,//返したいアクティビティをセット
-            hour,
-            minute,
-            false)
-    }
-
-    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
-        //
-    }
-}
+//
+//class TimePick : DialogFragment(), TimePickerDialog.OnTimeSetListener {
+//
+//
+//    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+//        //インスタンス
+//        val c = Calendar.getInstance()
+//        //最初に、システムの現在の時間と分を取得
+//        val hour = c.get(Calendar.HOUR_OF_DAY)
+//        val minute = c.get(Calendar.MINUTE)
+//
+//        return TimePickerDialog(
+//            activity,
+//            activity as ClockActivity?,//返したいアクティビティをセット
+//            hour,
+//            minute,
+//            false)
+//    }
+//
+//    override fun onTimeSet(view: TimePicker, hourOfDay: Int, minute: Int) {
+//        //
+//    }
+//}
